5c99d70754608566af4de6c15cc09fc7f7014bd8
Ubuntu <shrivardhanlimbkar@shri.fx1ni0uhg5ve1fh1v0czt1r2pa.ax.internal.cloudapp.net>
2021-03-18 21:57:24 +0000

    added new files

---
+++ hello.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
